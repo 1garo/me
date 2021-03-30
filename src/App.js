67a2c4f8f0d0103e99683f58df25a9a3da8f1630
1garo <alevardai@hotmail.com>
2021-03-30 11:29:05 -0300

    feat: adjust app to new personal blog post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './Components/sidebar'
+// import Introduction from './Components/introduction'
+// import About from './Components/about'
+// import Timeline from './Components/timeline'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page" className="App">
+          {/* <Header/> */}
+         	<Sidebar/>
+      </div>
+    );
+  }
 }
 
 export default App;
